91b79acfe977bd2533a6450fd978b3cf1b1a046f
George Tsamaladze <tsamaladze@gmail.com>
2022-07-30 14:06:15 +0300

    add logo

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,10 +8,13 @@ function Navbar() {
   return (
     <AppBar position="static">
       <Toolbar>
+        <div style={{ padding: '5px', marginRight: '5px' }}>
+          <img src="/LOGO5.png" alt="logo" style={{ width: '4rem', height: '4rem' }} />
+        </div>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <NavLink to="/vets">Поиск ветеринара</NavLink>
+          <NavLink to="/vets" style={{ textDecoration: 'none', color: 'inherit' }}>Поиск ветеринара</NavLink>
         </Typography>
-        <Button color="inherit"><NavLink to="auth">Авторизация</NavLink></Button>
+        <Button color="inherit"><NavLink to="auth" style={{ textDecoration: 'none', color: 'inherit' }}>Авторизация</NavLink></Button>
       </Toolbar>
     </AppBar>
   );
