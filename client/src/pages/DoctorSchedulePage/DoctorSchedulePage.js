69faeb0eec1da6045171d2c3e6c2077a3285c6b8
George Tsamaladze <tsamaladze@gmail.com>
2022-08-01 17:30:59 +0300

    bug fix

--- client/src/pages/DoctorSchedulePage/DoctorSchedulePage.js
+++ client/src/pages/DoctorSchedulePage/DoctorSchedulePage.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { errorShowAC } from '../../redux/actions/errorAction';
+import { getPetThunk } from '../../redux/actions/petActions';
 
 const fetchDocVisits = async (id) => {
   const response = await fetch(`http://localhost:3010/api/v1/doctors/${id}/schedule`);
@@ -17,11 +19,10 @@ const fetchDocVisits = async (id) => {
 function DoctorSchedulePage() {
   const doctor = useSelector((store) => store.user);
   const dispatch = useDispatch();
-  const today = new Date();
+
+  const navigate = useNavigate();
 
   const [schedule, setSchedule] = useState([]);
-
-  console.log(schedule);
 
   useEffect(() => {
     fetchDocVisits(doctor.id)
@@ -34,16 +35,39 @@ function DoctorSchedulePage() {
       })
       .catch((error) => dispatch(errorShowAC(error)));
   }, []);
+
+  const startVisithandler = async (id) => {
+    await dispatch(getPetThunk(id));
+    navigate('/visits/new');
+  };
+
   return (
-    <Container>
-      {schedule.length > 0 && schedule.map((el) => (
-        <Box sx={{ width: '100%', height: 'max-content', padding: '1rem' }}>
-          <Typography variant="h5">
-            {`${el.patient.first_name} ${el.patient.last_name}`}
-          </Typography>
-          <Typography variant="h6">
-            {`${el.Pet.name} ${el.Pet.specie}`}
-          </Typography>
+    <Container sx={{ padding: '1rem' }}>
+      {schedule.length > 0 && schedule.map((el, index) => (
+        <Box
+          key={index}
+          sx={{
+            height: 'max-content',
+            padding: '1rem',
+            border: '1px solid rgba(0,0,0,0.3)',
+            borderRadius: '10px',
+            marginBottom: '0.7rem',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Box>
+            <Typography variant="h5">
+              {`${el.patient.first_name} ${el.patient.last_name}`}
+            </Typography>
+            <Typography variant="h6">
+              {`${el.Pet.name} ${el.Pet.specie}`}
+            </Typography>
+          </Box>
+          <Box>
+            <Button onClick={() => { startVisithandler(el.Pet.id); }}>Начать прием</Button>
+          </Box>
         </Box>
       ))}
     </Container>
