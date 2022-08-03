7a42b9963cc7e2ab47c1995067e1cb8ad54c9eb0
Marina <kikkafikka90@gmail.com>
2022-08-03 13:19:48 +0400

    mail_function

--- client/src/pages/DocFind/DocFind.js
+++ client/src/pages/DocFind/DocFind.js
@@ -1,5 +1,5 @@
 import { TextField } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, Container } from '@mui/system';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FindSelectors from '../../components/FindSelectors/FindSelectors';
